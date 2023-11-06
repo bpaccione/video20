7d1a950db43789c5a42d74f1d6b2183dad93831f
bpaccione <143567666+bpaccione@users.noreply.github.com>
2023-11-06 17:26:17 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,11 +1,19 @@
-var video;
+video = document.querySelector("#player1");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
+	// initialize video element 
+	video.autoplay = false;
+	video.pause();
+	video.loop = false;
+	video.volume = document.querySelector("#slider").value / 100;
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").textContent = Math.round(video.volume * 100) + "%";
+});
 
