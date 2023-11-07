c5a785862ce66b14fcbba6e735a16d8cdf0987a0
bpaccione <143567666+bpaccione@users.noreply.github.com>
2023-11-07 17:43:28 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -17,3 +17,22 @@ document.querySelector("#play").addEventListener("click", function() {
 	document.querySelector("#volume").textContent = Math.round(video.volume * 100) + "%";
 });
 
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	console.log("Pause Video");
+	video.pause();
+	document.querySelector("#volume").textContent = Math.round(video.volume * 100) + "%";
+});
+
+document.querySelector("#slower").addEventListener("click", function(){
+	console.log("Video slowed up");
+	document.querySelector('video').playbackRate = .25;
+});
+
+document.querySelector("#faster").addEventListener("click", function(){
+	console.log("Video sped up");
+	document.querySelector('video').playbackRate = 2;
+});
+
+
+
